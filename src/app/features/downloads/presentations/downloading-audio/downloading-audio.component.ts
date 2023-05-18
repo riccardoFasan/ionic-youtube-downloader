ad2e0a7340babd4e67ed49dd98db24b1ce9d49f1
richard <fasanriccardo21@gmail.com>
2023-05-18 21:37:02 +0200

    feat: downloading item

--- src/app/features/downloads/presentations/downloading-audio/downloading-audio.component.ts
+++ src/app/features/downloads/presentations/downloading-audio/downloading-audio.component.ts
@@ -9,8 +9,34 @@ import { IonicModule } from '@ionic/angular';
   imports: [CommonModule, IonicModule],
   template: `
     <ion-item>
-      <ion-img [src]="download.info?.thumbnailUrl"> </ion-img>
-      {{ download.info?.title }}
+      <ng-container *ngIf="download.info; else noInfo">
+        <ion-thumbnail slot="start">
+          <ion-img
+            [alt]="download.info.title"
+            [src]="download.info.thumbnailUrl"
+          >
+          </ion-img>
+        </ion-thumbnail>
+        <ion-label>
+          {{ download.info.title }}
+        </ion-label>
+      </ng-container>
+      <ion-spinner slot="end"></ion-spinner>
+      <ng-template #noInfo>
+        <ion-thumbnail slot="start">
+          <ion-skeleton-text [animated]="true"></ion-skeleton-text>
+        </ion-thumbnail>
+        <ion-label>
+          <ion-skeleton-text
+            [animated]="true"
+            style="width: 85%;"
+          ></ion-skeleton-text>
+          <ion-skeleton-text
+            [animated]="true"
+            style="width: 75%;"
+          ></ion-skeleton-text>
+        </ion-label>
+      </ng-template>
     </ion-item>
   `,
   styles: [],
